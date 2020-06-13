db482470cff499cf969a708d8a9cdfa328fe3d3e
matthew-devonport <roarsintheeast@gmail.com>
2020-06-13 15:34:45 +1200

    inital commit

---
+++ client/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+class Header extends React.Component {
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+              
+                <div id="header">
+                    <img src ="img/desk.jpg"></img>
+                <p>hello</p>
+                </div>
+                
+            </React.Fragment>
+        )
+    }
+}
+
+export default Header
