eaf33c486ff0e14e39ca5c3616e2daa78b46b6f9
matthew-devonport <roarsintheeast@gmail.com>
2020-06-14 15:20:53 +1200

    basic draft team homepage started

--- client/components/Team.jsx
+++ client/components/Team.jsx
@@ -7,11 +7,21 @@ class Team extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Meet the team!</h1>
-                <div>Matthew Devonport</div>
-                <div>Lyondhur Picciarelli</div>
+<React.Fragment>
+                <h1>Meet the Team!</h1> 
+                <div className="servicesArea">               
+                <div className="servicesContent">
+                <img src="./img/matthew-devonport.jpg" alt="React Icon" className="teamPic"></img>
+                <p className="centerText">Matthew Devonport</p>
+                <p className="servicesInfo">Here is some text</p>
+                </div>
+                <div className="servicesContent">
+                <img src="./img/lyondhur-picciarelli.jpg" alt="Shopping Icon" className="teamPic"></img>   
+                <p className="centerText">Lyondhur Picciarelli</p>
+                <p className="servicesInfo">Here is some text</p>
+                </div> 
             </div>
+            </React.Fragment>
         )
     }
     
