ea88e723aef18214e9d8a1b418ecaced5f000162
matthew-devonport <roarsintheeast@gmail.com>
2020-06-14 14:59:11 +1200

    services homepage area draft done

--- client/components/Services.jsx
+++ client/components/Services.jsx
@@ -13,7 +13,7 @@ class Services extends React.Component {
                 <div className="servicesContent">
                 <img src="./img/react-icon.png" alt="React Icon" className="icons"></img>
                 <p className="centerText">Code a website using React</p>
-                <p className="servicesInfo">We can create a responsive desktop/mobile website using React</p>
+                <p className="servicesInfo">We can build a responsive desktop/mobile website using React</p>
                 </div>
                 <div className="servicesContent">
                 <img src="./img/shopping-icon.png" alt="Shopping Icon" className="icons"></img>   
@@ -23,14 +23,17 @@ class Services extends React.Component {
                 <div className="servicesContent">
                 <img src="./img/contract-icon.png" alt="Contract Icon" className="icons"></img>     
                 <p className="centerText">Contract us to code</p>
+                <p className="servicesInfo">Need your website design coded? We can do it!</p>
                 </div>
                 <div className="servicesContent">
                 <img src="./img/maintain-icon.png" alt="Maintain Icon" className="icons"></img>     
                 <p className="centerText">Maintain your website</p>
+                <p className="servicesInfo">Eliminate stress as we keep your website up to date with content</p>
                 </div>
                 <div className="servicesContent">
                 <img src="./img/customer-icon.png" alt="Customer Icon" className="icons"></img>     
                 <p className="centerText">Great customer support</p>
+                <p className="servicesInfo">Clear communication and support means great peace of mind</p>
                 </div>
             </div>
             </React.Fragment>
