a7c5e97da349ae4535e6aa7621624e5e0e2568ba
matthew-devonport <roarsintheeast@gmail.com>
2020-06-19 14:54:22 +1200

    changed team bios to nothing

--- client/components/Team.jsx
+++ client/components/Team.jsx
@@ -15,9 +15,6 @@ class Team extends React.Component {
                 <img src="./img/matthew-devonport.jpg" alt="React Icon" className="teamPic"></img>
                 <p className="teamTitle">Matthew Devonport</p>
                 <p className="teamInfo">Owner / Developer / Founder</p>
-                <p className="teamBio">Matthew has a passion for building applications. With 8 years experience in the tech and customer
-                    service industry, he strives to deliver 
-                </p>
                 <div className="teamIcons">
                 <i class="fab fa-facebook-square fa-lg facebookIcon"></i>
                 <i class="fab fa-instagram instaIcon fa-lg"></i>
@@ -28,9 +25,6 @@ class Team extends React.Component {
                 <img src="./img/lyondhur-picciarelli.jpg" alt="Shopping Icon" className="teamPic"></img>   
                 <p className="teamTitle">Lyondhur Picciarelli</p>
                 <p className="teamInfo">Design &amp; Systems Adviser</p>
-                <p className="teamBio">Matthew has a passion for building applications. With 8 years experience in the tech and customer
-                    service industry, he strives to deliver 
-                </p>
                 <div className="teamIcons">
                 <i class="fab fa-facebook-square fa-lg facebookIcon"></i>
                 <i class="fab fa-instagram instaIcon fa-lg"></i>
