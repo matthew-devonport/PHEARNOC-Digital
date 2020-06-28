09ec76b113e711bf3c32850da0d1f81be3fdf1de
matthew-devonport <roarsintheeast@gmail.com>
2020-06-28 14:32:02 +1200

    rough draft aboutus done

--- client/components/AboutUs.jsx
+++ client/components/AboutUs.jsx
@@ -16,14 +16,29 @@ class AboutUs extends React.Component {
                 <Header />
                 <div id="aboutUs">
                     <h1>Who are PHEARNOC Digital and what do we do?</h1>
-                    <img src="./img/logo.png" alt="Main Logo" className="popUpLogo centerImg"></img>
-                    <div className="aboutArea">
-                    <div className="centerText popUpBold">PHEARNOC Digital builds responsive websites for small/medium sizes businesses while nurturing these businesses through IT related problems.<br />
-                    This allows businesses to feel safe, guided and have a strong online presense to maximise their trading potenial.</div>
-                    <div>Our businesses is built on having strong integrial values. This is based on having excellent communication with customers, delivering professional products to them and finding solutions to problems that
+                    <div id="aboutArea">
+                    <div className="aboutUsInfo">
+                    <p className="aboutUsBold centerText">PHEARNOC Digital builds responsive websites for small/medium sizes businesses while nurturing these businesses through IT related problems.</p><br />
+                    <p className="centerText">This allows businesses to feel safe, guided and while having a strong online presense to maximise their trading potential.</p><br /><br />
+                    <p>Our businesses is built on having strong integrial values. This is based on having excellent communication with customers, delivering professional products to them and finding solutions to problems that
                     are presented to ourselves and our clients.
                     We want to build long lasting relationships with our clients and gain their trust by delivering on promises made in a timely manner.
+                    </p>
                     </div>
+                    <div className="logoFlex">
+                    <img src="./img/logo.png" alt="Main Logo" className="aboutUsLogo"></img>
+                    </div>
+                    <p className="aboutUsBold centerText">Want to partner with us? Get in touch!</p>   
+                    <div className="contactFlex">
+                <div className="contactColumn">
+               <img src="./img/phone.png" alt="React Icon" className="phoneLogo centerImg"></img>  
+               <a className="centerText">Phone Us</a>
+               </div>
+               <div className="contactColumn">  
+               <img src="./img/email.png" alt="React Icon" className="emailLogo centerImg"></img>
+               <a className="centerText">Email Us</a>
+               </div>
+               </div>
                     </div>
                     <Team />
                 </div>
