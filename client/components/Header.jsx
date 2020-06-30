a00452c9f67ff964b16a72b7cd3fe4967517d78d
matthew-devonport <roarsintheeast@gmail.com>
2020-06-30 19:26:44 +1200

    got claw up on desktop view

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -11,8 +11,8 @@ class Header extends React.Component {
             <React.Fragment>             
                 <div id="header">
                 <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
+                <img src="./img/claw.png" alt="React Icon" className="claw"></img>
                 </div>
-                
             </React.Fragment>
         )
     }
