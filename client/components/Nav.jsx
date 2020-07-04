26baea5824b613ee0c775d54889fb9eae2707ef3
matthew-devonport <roarsintheeast@gmail.com>
2020-07-04 13:12:13 +1200

    draft nav done

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -5,16 +5,18 @@ import { HashRouter as NavLink, Link } from 'react-router-dom';
 
 class Nav extends React.Component {
 
+    
     render() {
         return (
-            <div className="navBackground">
+            <div className="navBackground fixed">
                 <div className="leftNav fixed">
-                    <img src="./img/eyes.png" alt="Main Logo" className="leftLogos"></img>
+                    <div className="leftLinks">HOME</div>
                 </div>
-                <div className="flexBox">
-                    <div className="fixed">
-
-                    </div>
+                <div className="rightNav fixed">
+                <div className="rightLinks">ABOUT</div>
+                <div className="rightText">CONTACT:</div>
+                <div className="rightLinks">PHONE</div>
+                <div className="rightLinks">EMAIL</div>
                 </div>
             </div>
         )
