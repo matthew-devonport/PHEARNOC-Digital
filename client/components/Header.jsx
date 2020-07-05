1009b5ce6b2190b7eb62822ae1aaf1f1f7726f17
matthew-devonport <roarsintheeast@gmail.com>
2020-07-05 22:04:14 +1200

    changed header logo/titles to flex

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -8,12 +8,14 @@ class Header extends React.Component {
 
     render() {
         return (
-            <React.Fragment>             
+            <React.Fragment>
                 <div id="header">
-                <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
-                <div className="slogan">FEAR NOT!<br/>WE WILL BE YOUR DIGITAL PARTNER</div>
-                <img src="./img/eyes.gif" alt="React Icon" className="eyes"></img>
-                <img src="./img/claw.png" alt="React Icon" className="claw"></img>
+                    <div className="headerFlex">
+                        <div><img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img></div>
+                    <div className="slogan">FEAR NOT!<br />WE WILL BE YOUR DIGITAL PARTNER</div>
+                    </div>
+                    <img src="./img/eyes.gif" alt="React Icon" className="eyes"></img>
+                    <img src="./img/claw.png" alt="React Icon" className="claw"></img>
                 </div>
             </React.Fragment>
         )
