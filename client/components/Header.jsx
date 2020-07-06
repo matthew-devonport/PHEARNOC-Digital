fea2039f6bd89ce946cb3226ae1c1688f1336cc2
matthew-devonport <roarsintheeast@gmail.com>
2020-07-06 13:19:11 +1200

    changed a lot of margins with eyes/claws

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -14,8 +14,12 @@ class Header extends React.Component {
                         <div><img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img></div>
                     <div className="slogan">FEAR NOT!<br />WE WILL BE YOUR DIGITAL PARTNER</div>
                     </div>
-                    <img src="./img/eyes.gif" alt="React Icon" className="eyes"></img>
-                    <img src="./img/claw.png" alt="React Icon" className="claw"></img>
+                    <div className="headerPics">
+                    <div><img src="./img/eyes.gif" alt="React Icon" className="eyes"></img></div>
+                </div>
+                <div className="clawFlex">
+                <div ><img src="./img/claw.png" alt="React Icon" className="claw"></img></div>
+                </div>
                 </div>
             </React.Fragment>
         )
