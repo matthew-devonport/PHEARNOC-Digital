6e5ad3ed1ee9620df91f115a27076c2836a07bcc
matthew-devonport <roarsintheeast@gmail.com>
2020-07-08 13:45:44 +1200

    scaling to ipad properly

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -18,7 +18,7 @@ class Header extends React.Component {
                     <div><img src="./img/eyes.gif" alt="React Icon" className="eyes"></img></div>
                 </div>
                 <div className="clawFlex">
-                <div ><img src="./img/claw.png" alt="React Icon" className="claw"></img></div>
+                <div className="clawPosition"><img src="./img/claw.png" alt="React Icon" className="claw"></img></div>
                 </div>
                 </div>
             </React.Fragment>
