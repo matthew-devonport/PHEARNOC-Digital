4876b97e3a618af0cfb22edf17bcfd38fc9b8dbc
matthew-devonport <roarsintheeast@gmail.com>
2020-07-23 12:42:12 +1200

    team links working

--- client/components/Team.jsx
+++ client/components/Team.jsx
@@ -16,9 +16,9 @@ class Team extends React.Component {
                 <p className="teamTitle">Matthew Devonport</p>
                 <p className="teamInfo">Owner / Developer / Founder</p>
                 <div className="teamIcons">
-                <i className="fab fa-facebook-square fa-lg facebookIcon"></i>
-                <i className="fab fa-instagram instaIcon fa-lg"></i>
-                <i className="fab fa-linkedin linkedIcon fa-lg"></i>
+                <a href="https://www.facebook.com/MatthewDevonport" target="_blank"><i className="fab fa-facebook-square fa-lg facebookIcon"></i></a>
+                <a href="https://www.instagram.com/matthew_devonport" target="_blank"><i className="fab fa-instagram instaIcon fa-lg"></i></a>
+                <a href="https://www.linkedin.com/in/matthew-devonport" target="_blank"><i className="fab fa-linkedin linkedIcon fa-lg"></i></a>
                 </div>  
                 </div>
                 <div className="teamContent">
@@ -26,9 +26,9 @@ class Team extends React.Component {
                 <p className="teamTitle">Lyondhur Picciarelli</p>
                 <p className="teamInfo">Design &amp; Systems Adviser</p>
                 <div className="teamIcons">
-                <i className="fab fa-facebook-square fa-lg facebookIcon"></i>
-                <i className="fab fa-instagram instaIcon fa-lg"></i>
-                <i className="fab fa-linkedin linkedIcon fa-lg"></i>
+                <a href="https://www.facebook.com/lyondhur" target="_blank"><i className="fab fa-facebook-square fa-lg facebookIcon"></i></a>
+                <a href="https://www.instagram.com/lyondhur" target="_blank"><i className="fab fa-instagram instaIcon fa-lg"></i></a>
+                <a href="https://www.linkedin.com/in/lyondhur" target="_blank"><i className="fab fa-linkedin linkedIcon fa-lg"></i></a>
                 </div>         
                 </div> 
             </div>
