736fd9b27697cf1ff0de5748df9c76a474227dce
matthew-devonport <roarsintheeast@gmail.com>
2020-09-24 12:25:58 +1200

    testing content on popup works

--- tests/PopUpBox.test.js
+++ tests/PopUpBox.test.js
@@ -10,4 +10,9 @@ import PopUpBox from '../client/components/PopUpBox'
       wrapper = shallow(<PopUpBox togglePopup={togglePopup} content={'Test Text'} />)
   
 
+      test('Content is displayed in the right place', () => {
+        expect.assertions(1)
+        let content = wrapper.find('#popup-content')
+        expect(content.text()).toBe('Test Text')
+      })
 })
