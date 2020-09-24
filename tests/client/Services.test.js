93b9ebc0bae49b9bf813d82f1fd70509dc9279c9
matthew-devonport <roarsintheeast@gmail.com>
2020-09-24 13:19:44 +1200

    clicking learnMore calls popup test working. 5 working tests

--- tests/client/Services.test.js
+++ tests/client/Services.test.js
@@ -7,4 +7,11 @@ describe('<Services /> Component', () => {
     let wrapper, instance
       wrapper = shallow(<Services />)
       instance = wrapper.instance()
+       
+      test('Clicking learnMore calls popup', () => {
+        expect.assertions(1)
+        let button = wrapper.find('#learnMore')
+        button.simulate('click')
+        expect(instance.state.popupIsShowing).toBeTruthy()
+      })  
 })
