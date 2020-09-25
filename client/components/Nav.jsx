2aa7dbc3c5e0a9b811aab7c7f72baa254ead946c
matthew-devonport <roarsintheeast@gmail.com>
2020-09-25 17:51:47 +1200

    added AboutNav test

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -14,7 +14,7 @@ class Nav extends React.Component {
                 <div className="contactNav">
                 <div className="rightText">CONTACT:</div>
                 <div className="rightIcons"><a href="tel:02041988333"><i className="fas fa-phone-alt fa-lg"></i></a></div>
-                <div className="rightIcons"><a href="mailto:matthew@phearnoc.com"><i className="fas fa-envelope-open-text fa-lg"></i></a></div>
+                <div className="rightIcons"><a id="mail" href="mailto:matthew@phearnoc.com"><i className="fas fa-envelope-open-text fa-lg"></i></a></div>
                 
                 </div>
                 </div>
